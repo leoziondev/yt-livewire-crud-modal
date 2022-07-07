b7ee3c2352c182a7fa63440b444fd97c00a931dd
LeoZion <leozionsuporte@gmail.com>
2022-07-07 20:43:16 -0300

    get data items and paginate

--- resources/views/livewire/items.blade.php
+++ resources/views/livewire/items.blade.php
@@ -1,3 +1,30 @@
-<div>
-    Comming from the livewire component
+<div class="bg-white">
+    <div class="p-4 text-xl font-bold">Items</div>
+    <div>
+        <table class="table-auto w-full">
+            <thead class="bg-gray-100">
+                <tr class="text-left">
+                    <th class="text-md font-bold text-gray-700 p-4">ID</th>
+                    <th class="text-md font-bold text-gray-700 p-4">Name</th>
+                    <th class="text-md font-bold text-gray-700 p-4">Price</th>
+                    <th class="text-md font-bold text-gray-700 p-4">Status</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($items as $item)
+                <tr class="border-t border-gray-100">
+                    <td class="px-4 py-2">{{ $item->id }}</td>
+                    <td class="px-4 py-2">{{ $item->name }}</td>
+                    <td class="px-4 py-2">{{ number_format($item->price, 2)}}</td>
+                    <td class="px-4 py-2">{{ $item->status ? 'Active' : 'Not-Active' }}</td>
+                    <td class="px-4 py-2 flex justify-end"> edit delete</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <div class="p-4">
+            {{ $items->links() }}
+        </div>
+    </div>
 </div>
