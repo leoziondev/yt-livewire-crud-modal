fd1a43bfd0f3c1d24f2dc5e6dfe930ee177f64c4
LeoZion <leozionsuporte@gmail.com>
2022-07-07 19:01:47 -0300

    route page and link items

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/items', function () {
+        return view('items');
+    })->name('items');
 });
